088381110cf1426df2bb720237ba2574ff0c484d
i.sakharov <i731901@gmail.com>
2024-06-15 11:37:04 +0300

    file system reinstalled

--- bin/index.js
+++ bin/index.js
@@ -8,17 +8,17 @@ const program = new Command();
 program
   .version("0.1.0")
   .description("Create your SQL base and find users.")
-  .argument("<login>")
-  .argument("<password>")
-  .option("-d, --data <datafilepath>")
-  .option("-u, --user <userfilepath>")
-  .option("-f, --format <type>", "output format")
+  //.argument("<login>")
+  //.argument("<password>")
+  //.option("-d, --data <datafilepath>")
+  //.option("-u, --user <userfilepath>")
+  //.option("-f, --format <type>", "output format")
   .helpOption("-h, --help", "output usage information")
-  .action((datafilepath, userfilepath) => {
-    console.log(makeBase(datafilepath, userfilepath));
-  })
-  .action((login, password) => {
-    console.log(findUser(login, password));
-  });
+  //.action((datafilepath, userfilepath) => {
+  //  console.log(makeBase(datafilepath, userfilepath));
+  //})
+  //.action((login, password) => {
+  //  console.log(findUser(login, password));
+  //});
 
 program.parse();
